7e4f11196a1bd4e4a9b246d5b237d96191c89d3b
Wisdom <wisdomscode@gmail.com>
2024-01-25 22:31:14 +0100

    GET and POST requests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,76 @@
-console.log('Hello world')
+const express = require('express' );
+const app = express();
 
-// to use uuid
-const { v4: uuidv4 } = require('uuid');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-console.log(uuidv4());
+const PORT = 3000;
+
+
+customers = [
+    {
+        "name": "John Smith",
+        "email": "john@gmail.com",
+    },
+    {
+        "name": "Ezekiel",
+        "email": "eze@gmail.com",
+    },
+]
+
+
+json = {
+    "name": "Wisdom ugochukwu",
+    "phoneNumber": "0802345564",
+    "city": "Abuja",
+    "favoriteNumber": [
+        1,
+        2,
+        4,
+        6
+    ],
+    "favoriteColors": [
+        "Blue",
+        "White",
+        "Orange"
+    ],
+    "favoritePeople": [
+        {
+            "name": "Anuri",
+            "relationship": "wife"
+        },
+        {
+            "name": "Egochukwu",
+            "relationship": "parent"
+        }
+    ]
+}
+ 
+// using GET method
+app.get('/', (req, res) => {
+    res.send("Welcome to my website");
+});
+
+app.get('/profile', (req, res) => {
+    res.send({'data':json, 'favPeople':json.favoritePeople});
+});
+
+app.get('/api/customers', (req, res) => {
+    res.send({'customers':customers});
+});
+
+// send customer to backend
+app.post('/api/customers', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+
+// using POST method
+app.post('/', (req, res) => {
+    res.send('This is used to make POST request!!!');
+});
+
+app.listen(PORT, () =>{
+    console.log(`App listening on port ${PORT}`);
+})
---
+++ src/lesson1.js
@@ -0,0 +1,6 @@
+console.log('Hello world')
+
+// to use uuid
+const { v4: uuidv4 } = require('uuid');
+
+console.log(uuidv4());
